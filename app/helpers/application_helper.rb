1d702305aaba67a01cb18bd2f24d96249e622e39
eSilvac <esilva1995@gmail.com>
2017-01-12 02:07:26 -0500

    Pequña correccion con heroku

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
+require 'redcarpet/render_strip'
+
 module ApplicationHelper
+
+
 	def markdown(text)
 		markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
 	                                      no_intra_emphasis: true,
